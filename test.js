dc9b1acfee7c659485105878c9cff9ae93029ea1
minji <minji@dable.io>
2020-11-26 18:01:32 +0900

    MODIFY: paging 클릭 기능 추가, current item detect 추가, 변수명 리팩토링
    - 페이지 바 클릭으로 이동시 setInterval과 꼬임. 다시 실행.
    - 현재 네비게이션 바 클릭시 썸네일 zoom 이 의도대로 안 됨.
    - pause 상태에서 페이지 바를 누르면 버튼은 start 인 채로 파란 bar 는 차오르는데 bar 가 가득차도 다음 페이지로 넘어가진 않음.
    - 변수명 snake로 변경

--- test.js
+++ test.js
@@ -11,130 +11,115 @@
     });
   });
 
-  /* paging view */
-  var pagings = '<div class="pagings"></div>';
-  $('.pages').after(pagings);
+  /* view */
+  var pagings_el = '<div class="pagings"></div>';
+  var paging_el = '';
+  var total_page = $('.page-position').length;
 
-  var paging = '';
-  var total_page = $('.page-position').length; //set total pages
+  $('.pages').after(pagings_el);
   for (var i = 0; i < total_page; i++) {
-    paging += '<span class="paging" paging-idx= "' + i + '"><i class="bar"></i></span>';
+    paging_el += '<span class="paging" paging-idx= "' + i + '"><i class="bar"></i></span>';
   }
-  $('.pagings').append(paging);
+  $('.pagings').append(paging_el);
+  
+  var pause_el = '<button class="pause" onclick="onClickPauseButton()">pause</button>'; 
+  $('.pagings').after(pause_el);
 
-  /* paging function */
-  $('.paging').each(function (index) {
-    $(this).on('click', function () {
 
-      // 페이지 변경
-      $('.list').trigger('swipe_page', index + 1);
-
-      // swipe 그 시점부터 다시 활성화
-      // stopAutoSwipeTimer(); // stop이 아니라 해당 위치의 이후 페이지에만 auto되도록...
-
-      // 썸네일 zoom 애니메이션
-      var pagingIndex = $(this).attr('paging-idx');
-      $('.item').removeClass('current');
-      $('.item[data-idx=' + pagingIndex + ']').addClass('current');
-
-      // paging 애니메이션
-      // 이미 current인 paging은 클릭해도 변함없음
-      $(this).siblings('.paging').removeClass('current passed');
-      $(this).prevAll('.paging').addClass('passed');
-      $(this).removeClass('passed').addClass('current');
-
-    });
-  });
-
-  /* pause view */
-  var idx = 0; // current_page
-
-  var pause = '<button class="pause" onclick="onClickPauseButton()">pause</button>';
-  $('.pagings').after(pause);
-
-  function fixCurrentPagingBarWidth($curr_paging) {
-    var currPagingBar = $curr_paging.find('.bar');
-    var currPagingBarWidth = currPagingBar.css('width')
-    currPagingBar.css({
-      'width': currPagingBarWidth,
-    });
-  }
-  function resetPagingBarWidth($curr_paging){
-    var currPagingBar = $curr_paging.find('.bar');
-  };
-
-  /* pause function */
+  var curr_idx = 0; // current_page_index
+  var $pause_button = $('.pause');
   var is_playing = true;
-  function onClickPauseButton() { // toggle
-//     1. 버튼 텍스트 바꾸기, 2. progressbar 너비 유지, 다시 autoSwipe 시작.
-    var pauseButton = $('.pause');
-    var $curr_paging = $('.paging:eq(' + idx + ')');
+
+  $('.paging').on('click', function () {
+    var curr_paging_idx = $(this).index();
+    curr_idx = curr_paging_idx;
+    gotoPage(curr_paging_idx)
+    stopAutoSwipeTimer();
     
-    // toggle
-    if (is_playing) {  // playing -> pause
-      fixCurrentPagingBarWidth($curr_paging);
-      pauseButton.html('start');
+    $pause_button.html('pause');
+    startAutoSwipeTimer();
+    is_playing = true;
+  });
+  
+  function fixCurrThumbnailScale($curr_item) {
+    var curr_thumbnail = $curr_item.find('.thumbnail');
+    var curr_thumbnail_scale = curr_thumbnail.css('transform');
+    curr_thumbnail.css({
+      'transform': curr_thumbnail_scale,
+    });
+  }
+  function fixCurrPagingBarWidth($curr_paging) {
+    var curr_paging_bar = $curr_paging.find('.bar');
+    var curr_paging_bar_width = curr_paging_bar.css('width');
+    curr_paging_bar.css({
+      'width': curr_paging_bar_width,
+    });
+  }
+
+  function onClickPauseButton() { // toggle
+    var $curr_item = $('.item:eq(' + curr_idx + ')');
+    var $curr_paging = $('.paging:eq(' + curr_idx + ')');
+    if (is_playing) {  // playing -> pause      
+      fixCurrThumbnailScale($curr_item);
+      fixCurrPagingBarWidth($curr_paging);
+      $pause_button.html('start');
       stopAutoSwipeTimer();
+      $curr_item.removeClass('current'); // scale이 멈추긴 하는데 다시 실행이 안됨
       $curr_paging.removeClass('current');
       is_playing = false;
     } else { // pause -> play
-      pauseButton.html('pause');
+      $pause_button.html('pause');
       startAutoSwipeTimer();
+      $curr_item.addClass('current');
       $curr_paging.addClass('current');
       is_playing = true;
     }
   }
 
-  var paging_timer = null;
-  /* autoswipe */
-  function startAutoSwipeTimer() {
-    console.log("running startAutoSwipeTimer")
-    paging_timer = setInterval(nextPage, 5000);
-  }
-
-  
-  function gotoPage(idx){
-    console.log('running gotoPage: ', idx)
-    $('.list').trigger('swipe_page', idx + 1);
-    $('.paging .bar').removeAttr('style');
-    applyPagingClass(idx);
-    applyItemClass(idx);
-  }
-
-  function applyPagingClass(idx) {
+  function applyPagingClass(curr_idx) {
     var $all_pagings = $('.paging');
-    var $current_paging = $('.paging:eq(' + idx + ')');
-    var $prev_pagings = $current_paging.prevAll('.paging');
-    // var $next_pagings = $current_paging.nextAll('.paging');
+    var $curr_paging = $('.paging:eq(' + curr_idx + ')');
+    var $prev_pagings = $curr_paging.prevAll('.paging');
     $all_pagings.removeClass('current passed');
-    $current_paging.addClass('current');
+    $curr_paging.addClass('current');
     $prev_pagings.addClass('passed');
   }
 
-  function applyItemClass(idx) {
+  function applyItemClass(curr_idx) {
     var $all_items = $('.item');
-    var $current_item = $('.item' + idx);
+    var $curr_item = $('.item' + curr_idx);
     $all_items.removeClass('current');
-    $current_item.addClass('current');
+    $curr_item.addClass('current');
   }
-  
-  function nextPage() {
-    console.log("running nextPage");
-    idx = ++idx % total_page; // loop
-    gotoPage(idx);
-  }
-  gotoPage(idx);
-  
-  startAutoSwipeTimer();
 
+  var paging_timer = null;
+  /* autoswipe */
+  function startAutoSwipeTimer() {
+    paging_timer = setInterval(nextPage, 3000);
+  }
   function stopAutoSwipeTimer() {
     clearTimeout(paging_timer);
   }
+  
+  function gotoPage(curr_idx){
+    $('.list').trigger('swipe_page', curr_idx + 1);
+    $('.paging .bar').removeAttr('style');
+    console.log('running gotoPage: ', curr_idx)
+    applyItemClass(curr_idx);
+    applyPagingClass(curr_idx);
+  }
+  
+  function nextPage() {
+    curr_idx = ++curr_idx % total_page; // loop
+    console.log("running nextPage", curr_idx);
+    gotoPage(curr_idx);
+  }
+  gotoPage(curr_idx);
+  startAutoSwipeTimer();
 
-  // TODO: 페이지 바 클릭으로 이동시 setInterval과 꼬임. 다시 실행.
-  // TODO: 현재 네비게이션 바 클릭시 썸네일 zoom 이 의도대로 안 됨.
-    // pause 상태에서 페이지 바를 누르면 버튼은 start 인 채로 파란 bar 는 차오르는데 bar 가 가득차도 다음 페이지로 넘어가진 않음.
-    // 위 경우에 대한 시나리오 정리 필요.
+  // TODO: pause에서 resume하면 섬네일 zoom이 다시 동작하지 않음
+  // TODO: pause 누르면 current 클래스가 사라지면서 텍스트도 사라짐. 텍스트 유지하는 방법 고민
+  // TODO: pause 눌렀다가 다시 실행하면, transition이 남은 시간이 아닌 최초값 기준 5초동안 실행됨. -> javascript로 실행해야 하나? 아니면 남은 초도 함께 저장하는 방법?
   // TODO: 코드 정리 깔끔하게
 
   //start 할 때도 현재 bar 를 잘 찾아와서 current class를 붙인다.
