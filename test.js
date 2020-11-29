b85000a6867b48460b264ecdd9d9b9c8d2fd969a
minji <minji@dable.io>
2020-11-30 01:44:52 +0900

    BUGFIX: transition duration 가변적으로 변경
    - pause 눌렀다가 다시 실행하면, transition이 남은 시간이 아닌 최초값 기준 5초동안 실행됨

--- test.js
+++ test.js
@@ -26,14 +26,17 @@
   $('.pagings').after(pause_el);
 
 
+  /* function */
   var curr_idx = 0; // current_page_index
-  var $pause_button = $('.pause');
+  var init_time = 5; // sec
+  var set_duration = null;
   var is_playing = true;
+  var $pause_button = $('.pause');
 
   $('.paging').on('click', function () {
     var curr_paging_idx = $(this).index();
     curr_idx = curr_paging_idx;
-    gotoPage(curr_paging_idx)
+    gotoPage(curr_idx)
     stopAutoSwipeTimer();
     
     $pause_button.html('pause');
@@ -41,19 +44,41 @@
     is_playing = true;
   });
   
+  function setDurationTimer() {
+    duration_timer = setInterval(function(){
+      init_time --;
+      if(init_time === 0){
+        init_time = 5;
+      }
+    }, 1000);
+  }
+  function pauseDurationTimer() {
+    clearInterval(duration_timer);
+  }
+
   function fixCurrThumbnailScale($curr_item) {
-    var curr_thumbnail = $curr_item.find('.thumbnail');
-    var curr_thumbnail_trans = curr_thumbnail.css('transform');
+    var $curr_thumbnail = $curr_item.find('.thumbnail');
+    var curr_thumbnail_trans = $curr_thumbnail.css('transform');
     var curr_thumbnail_scale = curr_thumbnail_trans.split('(')[1].split(')')[0].split(',')[0]
-    curr_thumbnail.css({
-      'transform': 'scale('+ curr_thumbnail_scale +')'
+    $curr_thumbnail.css({
+      'transform': 'scale('+ curr_thumbnail_scale +')',
     });
   }
   function fixCurrPagingBarWidth($curr_paging) {
-    var curr_paging_bar = $curr_paging.find('.bar');
-    var curr_paging_bar_width = curr_paging_bar.css('width');
-    curr_paging_bar.css({
+    var $curr_paging_bar = $curr_paging.find('.bar');
+    var curr_paging_bar_width = $curr_paging_bar.css('width');
+    $curr_paging_bar.css({
       'width': curr_paging_bar_width,
+    });
+  }
+  function updateTransitionDuration($curr_paging) {
+    var $all_thumbnail = $('.item .thumbnail');
+    var $curr_paging_bar = $curr_paging.find('.bar');
+    $all_thumbnail.css({
+      'transition-duration': init_time + 's',
+    });
+    $curr_paging_bar.css({
+      'transition-duration': init_time + 's',
     });
   }
 
@@ -64,16 +89,19 @@
       fixCurrThumbnailScale($curr_item);
       fixCurrPagingBarWidth($curr_paging);
       $pause_button.html('start');
-      stopAutoSwipeTimer();
       $curr_item.removeClass('current');
       $curr_paging.removeClass('current');
       is_playing = false;
+      stopAutoSwipeTimer();
+      pauseDurationTimer();
     } else { // pause -> play
+      updateTransitionDuration($curr_paging);
       $pause_button.html('pause');
-      startAutoSwipeTimer();
       $curr_item.addClass('current');
       $curr_paging.addClass('current');
       is_playing = true;
+      startAutoSwipeTimer();
+      setDurationTimer();
     }
   }
 
@@ -101,6 +129,7 @@
   }
 
   var paging_timer = null;
+
   /* autoswipe */
   function startAutoSwipeTimer() {
     paging_timer = setInterval(nextPage, 5000);
@@ -110,6 +139,7 @@
   }
   
   function gotoPage(curr_idx){
+    init_time = 5;
     $('.list').trigger('swipe_page', curr_idx + 1);
     $('.paging .bar').removeAttr('style');
     $('.thumbnail').removeAttr('style');
@@ -117,17 +147,19 @@
     applyNameClass(curr_idx);
     applyPagingClass(curr_idx);
   }
-  
+
   function nextPage() {
     curr_idx = ++curr_idx % total_page; // loop
     gotoPage(curr_idx);
   }
+
   gotoPage(curr_idx);
   startAutoSwipeTimer();
+  setDurationTimer();
 
-  // TODO: animation -> transition 과정에서 name의 사라지는 효과들 사라짐.
-  // TODO: pause 눌렀다가 다시 실행하면, transition이 남은 시간이 아닌 최초값 기준 5초동안 실행됨. -> javascript로 실행해야 하나? 아니면 남은 초도 함께 저장하는 방법?
+  // TODO: restart하면 해당 페이지의 autoSwipe가 5초로 고정돼 멈춤, init_time으로 변경하면 다음 페이지들에서 오류 발생
   // TODO: 잊고있던 좌우 arrow 추가, trigger 기능
+  // TODO: animation -> transition 과정에서 name의 사라지는 효과들 사라짐. (optional)
   // TODO: 코드 정리 깔끔하게
 
   //start 할 때도 현재 bar 를 잘 찾아와서 current class를 붙인다.
